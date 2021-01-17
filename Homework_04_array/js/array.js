5dcf0e12ce4c19d4c3c09c820c906630f4f490c7
IvanPetkovski <76570904+IvanPetkovski@users.noreply.github.com>
2021-01-17 15:34:55 +0100

    Add files via upload

---
+++ Homework_04_array/js/array.js
@@ -0,0 +1,22 @@
+// ******* Homework 01 -  function TELL A STORY *******
+console.log("================\n Homework 01 \n -function TELL A STORY \n================ ")
+
+function tellStory() {
+    let array = ["Leonardo", "excited", "ice-skating"];
+    console.log(`Hey ${array[0]}, how are you this evening? \n I was really ${array[1]} to see you ${array[2]} again! \n It's been a long time since your last competition.
+    \n I hope you are ${array[1]} because we are thrilled to have you back ${array[0]}! \n See you soon.`);
+}
+tellStory();
+
+// ******* Homework 02 - SUM OF 5 NUMBERS IN ARRAY *******
+console.log("=================\n Homework 02 \n================= ")
+
+function sumNumbers() {
+    let arrayNumbers = [26, 13, 71, 268, 312];
+    console.log(arrayNumbers);
+    console.log(arrayNumbers[0] + arrayNumbers[1] + arrayNumbers[2] + arrayNumbers[3] + arrayNumbers[4]);
+}
+
+sumNumbers();
+
+
